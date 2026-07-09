710dea8e9e31f4ddde02783f08c2830a5aea0066
Fernando <111@qq.com>
2026-07-09 17:07:50 +0800

    不在pm2中配置环境变量

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -12,9 +12,6 @@ module.exports = {
       'server/storage/**/*'
     ],
     max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'production'
-    },
     log_date_format: 'YYYY-MM-DD HH:mm:ss',
     error_file: './logs/pm2-error.log',
     out_file: './logs/pm2-out.log',
